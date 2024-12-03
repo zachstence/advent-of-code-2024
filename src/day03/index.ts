59ff5731cb5d176eab6c238be9d2998706293c6b
Zachary Stence <zachstence@gmail.com>
2024-12-03 09:03:29 -0600

    day03 part1 solved

---
+++ src/day03/index.ts
@@ -0,0 +1,47 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const matches = input.matchAll(/mul\((\d+),(\d+)\)/g);
+
+  const total = [...matches].reduce((total, [_, a, b]) => {
+    const product = parseInt(a) * parseInt(b);
+    total += product;
+    return total;
+  }, 0);
+
+  return total.toString();
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input:
+          "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+        expected: "161",
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  // onlyTests: true,
+});
